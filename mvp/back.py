f9ce36740d51b4f85e5e2b952a65d765305c6d1a
Andrés Kusminsky <andres.kusminsky@gmail.com>
2023-12-11 14:52:36 -0300

    ajusto control flow para atajar error en metodo get_text

--- mvp/back.py
+++ mvp/back.py
@@ -33,18 +33,19 @@ def get_vocabItem(word):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     word_elem = soup.find('h3', {'class': 'headerWord'})
-    word_elem = word_elem.get_text().strip()
     translation_elem = soup.find('li', {'class': 'sense'})
-    translation_elem = translation_elem.get_text().strip()
-    translation_elem = translation_elem.split(";")[:3]
-    translation_elem = ";".join(translation_elem)
-    translation_elem = translation_elem.replace(",", "/")
-    vocabItem[word_elem] = translation_elem
-    
-    if vocabItem:
+
+    if word_elem is not None and translation_elem is not None:
+        word_elem = word_elem.get_text().strip()
+        translation_elem = translation_elem.get_text().strip()
+        translation_elem = translation_elem.split(";")[:3]
+        translation_elem = ";".join(translation_elem)
+        translation_elem = translation_elem.replace(",", "/")
+        vocabItem[word_elem] = translation_elem
         return vocabItem
     else:
-        return None
+        return 
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
